6807759343d4ef89833dca436d4f63f90bd6f9ec
Atharv <shingewaratharv@gmail.com>
2024-11-08 23:58:34 +0530

    basic change

--- config.py
+++ config.py
@@ -9,12 +9,22 @@ cfg.weight_decay = 1e-4
 cfg.patience = 25
 cfg.inference_threshold = 0.75
 
+# cfg.transunet = EasyDict()
+# cfg.transunet.img_dim = 512
+# cfg.transunet.in_channels = 3
+# cfg.transunet.out_channels = 128
+# cfg.transunet.head_num = 4
+# cfg.transunet.mlp_dim = 512
+# cfg.transunet.block_num = 8
+# cfg.transunet.patch_dim = 16
+# cfg.transunet.class_num = 1
+
 cfg.transunet = EasyDict()
 cfg.transunet.img_dim = 512
 cfg.transunet.in_channels = 3
-cfg.transunet.out_channels = 128
-cfg.transunet.head_num = 4
-cfg.transunet.mlp_dim = 512
-cfg.transunet.block_num = 8
+cfg.transunet.out_channels = 64
+cfg.transunet.head_num = 2
+cfg.transunet.mlp_dim = 256
+cfg.transunet.block_num = 4
 cfg.transunet.patch_dim = 16
 cfg.transunet.class_num = 1
